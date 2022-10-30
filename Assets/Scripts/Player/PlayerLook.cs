30217b494c426db074b23ba4f1564578ace93be4
smohan67 <siddhu.mohan6@gmail.com>
2022-10-30 18:27:13 -0500

    player final works, time to add player animations

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        private float rotX;
+        private float rotY;
+
+        public Camera Camera;
+    
+
+        void Update()
+        {
+            rotX = Input.GetAxis("Mouse Y");
+            rotY = Input.GetAxis("Mouse X");
+            Vector3 rotation = transform.localEulerAngles;
+            rotation.y += rotY;
+            transform.localRotation = Quaternion.AngleAxis(rotation.y, Vector3.up);
+
+
+            Vector3 camRotation = Camera.transform.localEulerAngles;
+            camRotation.x -= rotX;
+            Camera.transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.right);
+        }
+    }
+}
