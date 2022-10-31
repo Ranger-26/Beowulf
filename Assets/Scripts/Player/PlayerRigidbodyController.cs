c2edd1be219050f748b258284e2e5e01458b0fc1
smohan67 <siddhu.mohan6@gmail.com>
2022-10-30 21:08:06 -0500

    animations done

--- Assets/Scripts/Player/PlayerRigidbodyController.cs
+++ Assets/Scripts/Player/PlayerRigidbodyController.cs
@@ -13,20 +13,55 @@ namespace Player
         private Vector3 _movementVector;
         public Transform Orientation;
         public bool Grounded;
+
+        private Animator _animator;
         
         private void Start () {
             _rig = GetComponent<Rigidbody> ();
+            _animator = GetComponent<Animator>();
             _rig.freezeRotation = true;
         }
         private void Update () {
             _input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+            
             if (Input.GetButtonDown ("Jump") && Grounded) {
+                _animator.SetTrigger("Jump");
                 _rig.AddForce (Vector3.up * _jumpForce, ForceMode.Impulse);
             }
         }
         private void FixedUpdate () {
             _movementVector =
                 Orientation.transform.TransformDirection(new Vector3(_input.x * _speed, _rig.velocity.y, _input.y * _speed));
+            if (_input.x == 0 && _input.y == 0)
+            {
+                _animator.ResetTrigger("Run");
+                _animator.ResetTrigger("RunBackwards");
+                _animator.ResetTrigger("RunLeft");
+                _animator.ResetTrigger("RunRight");
+                _animator.SetTrigger("Idle");
+            }
+            else
+            {
+                _animator.ResetTrigger("Idle");
+                if (_input.y < 0)
+                {
+                    _animator.SetTrigger("RunBackwards");
+                }
+                else if (_input.y > 0)
+                {
+                    _animator.SetTrigger("Run");
+                }
+                else if (_input.x < 0)
+                {
+                    Debug.Log(_input.x);
+                    _animator.SetTrigger("RunLeft");
+                }
+                else if (_input.x > 0)
+                {
+                    Debug.Log(_input.x);
+                    _animator.SetTrigger("RunRight");
+                }
+            }
             _rig.velocity = new Vector3 (_movementVector.x, _rig.velocity.y, _movementVector.z);
         }
         
