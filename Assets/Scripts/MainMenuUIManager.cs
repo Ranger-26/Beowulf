7ab81624a2ca350d6a7299b4227886038a1a483a
smohan67 <siddhu.mohan6@gmail.com>
2022-11-01 23:55:49 -0500

    done with main menu

---
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("FightScene");
+    }
+}
